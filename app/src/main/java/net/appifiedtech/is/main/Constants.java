b2478255424a442e0bd7fef1d413dc489c0a1297
Priyabrat <ppadhy@regalix-inc.com>
2016-05-02 12:32:40 +0530

    drc4rf

--- app/src/main/java/net/appifiedtech/is/main/Constants.java
+++ app/src/main/java/net/appifiedtech/is/main/Constants.java
@@ -5,4 +5,7 @@ package net.appifiedtech.is.main;
  */
 public class Constants {
     public static final String URL_APPS_LIST = "http://javatechig.com/api/get_category_posts/?dev=1&slug=android";
+
+    // For HTTP Download
+    // http://www.androidhive.info/2012/04/android-downloading-file-by-showing-progress-bar/
 }
